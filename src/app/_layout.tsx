8e34e1f9c71b4e8b7c4a75b4b4bed7a0460f3a0a
Edis Leka <edisleka@yahoo.gr>
2025-08-18 16:47:55 +0300

    Clerk setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { Stack } from 'expo-router'
+
+const InitialLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='(app)' options={{ headerShown: false }} />
+    </Stack>
+  )
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <InitialLayout />
+    </ClerkProvider>
+  )
 }
